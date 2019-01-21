7f9afd7d8488a653711bfbb49c662ce0a11c7222
nhsoftwc <nhsoft.wc@outlook.com>
2019-01-21 11:53:01 +0800

    update

--- src/main/java/com/miaoshaproject/validator/ValidatorImpl.java
+++ src/main/java/com/miaoshaproject/validator/ValidatorImpl.java
@@ -2,15 +2,21 @@ package com.miaoshaproject.validator;
 
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+
 /**
  * @Author: Alan.W
  * @Date: 2019/1/21 10:37
  */
 public class ValidatorImpl implements InitializingBean {
 
+    private Validator validator;
+
     @Override
     public void afterPropertiesSet() throws Exception {
-
+        // 将hibernate validator通过工厂的初始化方式使其实例化
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
 }
