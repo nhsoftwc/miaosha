610187ce4b3c2ce68fbf27c411017bc4dc3a5e77
nhsoftwc <nhsoft.wc@outlook.com>
2019-01-21 10:58:09 +0800

    update

---
+++ src/main/java/com/miaoshaproject/validator/ValidatorImpl.java
@@ -0,0 +1,16 @@
+package com.miaoshaproject.validator;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * @Author: Alan.W
+ * @Date: 2019/1/21 10:37
+ */
+public class ValidatorImpl implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+}
